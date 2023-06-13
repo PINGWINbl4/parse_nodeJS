8607b018ae394cdeadac8d0fcf72a8925eaa3a73
PINGWINbl4 <mikmez01@gmail.com>
2023-06-13 09:34:17 +0500

    microfix

--- utils/parse.js
+++ utils/parse.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const xml2js = require('xml2js');
-const iconv = require('iconv-lite')
 
 async function parseXML(unzippedFileName){
     const parser = new xml2js.Parser();
-    const file = await fs.promises.readFile('./storage/' + unzippedFileName, 'utf-8')
+    const file = await fs.promises.readFile('./storage/' + unzippedFileName)
     const parsedContent = await parser.parseStringPromise(file);
     const list = parsedContent.ED807.BICDirectoryEntry;
     const currentBICList = []
@@ -12,7 +11,7 @@ async function parseXML(unzippedFileName){
         for(let accIndex in list[elementIndex].Accounts){
             currentBICList.push({
                 bic: list[elementIndex].$.BIC,
-                name: iconv.decode(list[elementIndex].ParticipantInfo[0].$.NameP, 'koi8-ru'),
+                name: list[elementIndex].ParticipantInfo[0].$.NameP,
                 corrAccount: list[elementIndex].Accounts[accIndex].$.Account
             })
         }
